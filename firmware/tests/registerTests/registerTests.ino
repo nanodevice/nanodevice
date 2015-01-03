cddbace3caeb586eee90e1e67c7a81b5b7c65c1a
Reto Stamm <reto@retostamm.com>
2015-01-02 16:47:43 -0800

    Added constants from datasheet, simplifications.

--- firmware/tests/registerTests/registerTests.ino
+++ firmware/tests/registerTests/registerTests.ino
@@ -32,13 +32,13 @@
 #include <SPI.h>
 
 // Board Pinouts
-static const int DAC_INV_CS     = 10; // DAC Pin 6
-static const int DAC_INV_LDAC   =  7; // DAC Pin 4
 static const int DAC_INV_CLR    =  5; // DAC PIN 5
+static const int DAC_INV_LDAC   =  7; // DAC Pin 4
+static const int DAC_INV_CS     = 10; // DAC Pin 6
 
 static const int ADC_INV_SYNC  = 1; // ADC Pin 20
 static const int ADC_INV_DRDY  = 3; // ADC Pin 14
-static const int ADC_INV_RESET = 4;
+static const int ADC_INV_RESET = 4; // ADC Pin
 static const int ADC_CLOCK_OUT = 6; // ADC Pin 21
 static const int ADC_INV_CS    = 9; // ADC Pin 15
 
@@ -49,12 +49,27 @@ static const byte ADC_REG_ADCON  = 0x02; // Control Register
 static const byte ADC_REG_DRATE  = 0x03; // Datarate
 static const byte ADC_REG_GPIO   = 0x04; // GPIO config
 static const byte ADC_REG_OFC0   = 0x05; // Calibration: Offset
-static const byte ADC_REG_OFC1   = 0x06;
-static const byte ADC_REG_OFC2   = 0x07;
+static const byte ADC_REG_OFC1   = 0x06; // Calibration: Offset
+static const byte ADC_REG_OFC2   = 0x07; // Calibration: Offset
 static const byte ADC_REG_FSC0   = 0x08; // Calibration: Scaling
-static const byte ADC_REG_FSC1   = 0x09;
-static const byte ADC_REG_FSC2   = 0x0A;
+static const byte ADC_REG_FSC1   = 0x09; // Calibration: Scaling
+static const byte ADC_REG_FSC2   = 0x0A; // Calibration: Scaling
 
+static const byte ACD_CMD_WAKEUP   = 0x00; // Complete SYNC and exit standby mode
+static const byte ADC_CMD_RDATA    = 0x01; // Read data
+static const byte ADC_CMD_RDATAC   = 0x03; // Read data continuously
+static const byte ADC_CMD_SDATAC   = 0x0F; // Stop read data continuous
+static const byte ADC_CMD_RREG     = 0x10; // Read from register. BITWISE OR with register  
+static const byte ADC_CMD_WREG     = 0x50; // Write to register.  BITWISE OR with register
+static const byte ADC_CMD_SELFCAL  = 0xF0; // Offset and Gain Self-Calibration
+static const byte ADC_CMD_SELFOCAL = 0xF1; // Offset Self-Calibration
+static const byte ADC_CMD_SELFGCAL = 0xF2; // Gain Self-Calibration
+static const byte ADC_CMD_SYSOCAL  = 0xF3; // System Offset Calibration
+static const byte ADC_CMD_SYSGCAL  = 0xF4; // System Gain Calibration
+static const byte ADC_CMD_SYNC     = 0xFC; // Synchronize the A/D Conversion
+static const byte ADC_CMD_STANDBY  = 0xFD; // Begin Standby Mode
+static const byte ADC_CMD_RESET    = 0xFE; // Resets to Power-Up Values
+static const byte ADC_CMD_WAKEUP   = 0xFF; // Completes SYNC and Exits Standby Mode
 
 static const byte ADC_MUX_VAL_TEMP    = 0x23; // AIN2, AIN3: Differential Temperature
 static const byte ADC_MUX_VAL_CURRENT = 0x01; // AIN0, AIN1: Differential Current
@@ -68,16 +83,13 @@ void setup() {
   Serial.begin(9600);
   Serial.println(F(""));
   
-  SPI.setClockDivider(SPI_CLOCK_DIV8); // 8, 16
-  int mode = SPI_MODE1;
-  SPI.setDataMode(mode);
   SPI.begin();
-  SPI.setDataMode(mode);
+  SPI.setDataMode(SPI_MODE1);
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
 
   pinMode(DAC_INV_CS, OUTPUT);
   digitalWrite(DAC_INV_CS, 1);
 
-  SPI.setClockDivider(SPI_CLOCK_DIV8);
 
   // ADC Clock
   analogWriteFrequency(ADC_CLOCK_OUT, 375000*20); // 8MHz
@@ -96,10 +108,8 @@ void setup() {
 void reset () {
   digitalWrite(ADC_INV_SYNC, 1);
   digitalWrite(ADC_INV_CS, 1);
-  digitalWrite(ADC_INV_RESET, 1);
-  delayMicroseconds(1000);
   digitalWrite(ADC_INV_RESET, 0);
-  delayMicroseconds(1000);
+  delayMicroseconds(10);
   digitalWrite(ADC_INV_RESET, 1);
   delayMicroseconds(8000);  // Self Calibration takes 3.8ms. This is twice as much.
 
@@ -108,33 +118,34 @@ void reset () {
   digitalWrite(ADC_INV_CS, 1); 
 
   digitalWrite(ADC_INV_CS, 0); 
-  SPI.transfer(0x00);  // Complete sync and exit standby mode
+  SPI.transfer(ADC_CMD_WAKEUP);  // Complete sync and exit standby mode
   digitalWrite(ADC_INV_CS, 1); 
   delayMicroseconds(100000);   
 }
 
-byte s, m, a, d, i;
+byte stat, mux, adcon, datarate, gpio;
 char charBuf[512];
 
 void loop() {
-  waitForDRDYPulse();
+  waitForDRDYPulse(); // Wait fo drdy - this appears to increase stability
   digitalWrite(ADC_INV_CS, 0); 
-  SPI.transfer(0b00010000); // Read Register 0
+  SPI.transfer(ADC_CMD_RREG | 0); // Read Register 0
   SPI.transfer(5-1); // ... and the following 4
-  delayMicroseconds(30); // Wait for t6 - 50x tclkin = 125ns
-  // Wait fo drdy as well
-  waitForDRDYPulse();
-  s = SPI.transfer(0b00000000); // Read it
-  m = SPI.transfer(0b00000000); // Read it
-  a = SPI.transfer(0b00000000); // Read it
-  d = SPI.transfer(0b00000000); // Read it
-  i = SPI.transfer(0b00000000);
+  
+  delayMicroseconds(1); // Wait for t6 - 50x tclkin = 125ns
+  waitForDRDYPulse();   // Wait fo drdy as well - this appears to increase stability
+  
+  stat     = SPI.transfer(0x00); // Read it
+  mux      = SPI.transfer(0x00); // Read it
+  adcon    = SPI.transfer(0x00); // Read it
+  datarate = SPI.transfer(0x00); // Read it
+  gpio     = SPI.transfer(0x00); // Read it
   digitalWrite(ADC_INV_CS, 1); 
           
-  sprintf(charBuf, "STAT 0x%2x MUX  0x%2x CON  0x%2x RATE 0x%2x IO   0x%2x", s, m, a, d, i );
+  sprintf(charBuf, "STAT 0x%2x MUX  0x%2x ADCON  0x%2x DATARATE 0x%2x  GPIO 0x%2x", 
+                    stat,      mux,       adcon,       datarate,       gpio );
   Serial.println(charBuf);
-  reset();
-  delayMicroseconds(10000); 
+  delayMicroseconds(100000); 
 }
 
 void waitForDRDYLow() {
